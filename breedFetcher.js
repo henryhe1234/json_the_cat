e0074c4b6a903550a1e62a3458f31f2d5ae790ce
henryhe1234 <henryhe1123@gmail.com>
2020-08-28 00:19:49 +0000

    add testing file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,23 @@
 const argv = process.argv.slice(2);
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`, (error, response, body) => {
-  console.log('err:', error);
-  console.log('status code:', response && response.statusCode);
-  if (response.statusCode !== 200) {
-    console.log('some error with website');
-  }
-  const data = JSON.parse(body);
-  console.log(data);
-  if (data) {
-    console.log('The breed not found');
-  } else {
-    console.log(data[0]);
-  }
-});
+
+const fetchBreedDescription = (breedName, callback) => {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // console.log('err:', error);
+    // console.log('status code:', response && response.statusCode);
+    const data = JSON.parse(body);
+    const dataObj = data[0];
+    // console.log(data);if()
+    if(dataObj){
+
+      callback(error, dataObj.description);
+    }else{
+      console.log('invalid breed');
+    }
+
+    
+
+  });
+};
+module.exports = { fetchBreedDescription };
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,23 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. "
+      // compare returned description
+      assert.equal(expectedDesc, desc);
+      done();
+    });
+  });
+  it('it should return false when invalid/non-existent breed is passed in',(done)=>{
+    fetchBreedDescription('aaaaaa',(err,desc)=>{
+      assert.equal(err,null);
+      assert.equal(desc,null);
+    })
+    done();
+  })
+});
